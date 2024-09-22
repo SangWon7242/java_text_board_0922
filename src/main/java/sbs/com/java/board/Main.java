6e747564033b90f5a51b64a40fae683c4a0c9d72
SangWon7242 <gotkdqja@gmail.com>
2024-09-22 09:32:56 +0900

    명령어 입력받기

--- src/main/java/sbs/com/java/board/Main.java
+++ src/main/java/sbs/com/java/board/Main.java
@@ -1,9 +1,20 @@
 package sbs.com.java.board;
 
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("== [텍스트 게시판] ==");
+    Scanner sc = new Scanner(System.in);
+    System.out.println("== 텍스트 게시판 ==");
+
     System.out.println("== 게시판을 시작합니다. ==");
+    System.out.print("명령어 ) ");
+    String cmd = sc.nextLine();
+
+    System.out.printf("입력받은 명령어 : %s\n", cmd);
+
     System.out.println("== 게시판을 종료합니다. ==");
+
+    sc.close();
   }
 }
