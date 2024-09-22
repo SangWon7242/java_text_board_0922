30c7f23ff31c171749a2a674a473f0d91353907e
SangWon7242 <gotkdqja@gmail.com>
2024-09-22 09:29:15 +0900

    시작 메세지 출력

--- src/main/java/sbs/com/java/board/Main.java
+++ src/main/java/sbs/com/java/board/Main.java
@@ -2,6 +2,8 @@ package sbs.com.java.board;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println("== [텍스트 게시판] ==");
+    System.out.println("== 게시판을 시작합니다. ==");
+    System.out.println("== 게시판을 종료합니다. ==");
   }
 }
