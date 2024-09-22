2000d11b9903cfb6a804c8f5e44084811988ad1a
SangWon7242 <gotkdqja@gmail.com>
2024-09-22 10:11:50 +0900

    게시물 등록시마다 id값이 증가 되도록

--- src/main/java/sbs/com/java/board/Main.java
+++ src/main/java/sbs/com/java/board/Main.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+    int lastArticleId = 0;
+
     System.out.println("== 텍스트 게시판 ==");
 
     System.out.println("== 게시판을 시작합니다. ==");
@@ -20,7 +22,7 @@ public class Main {
         System.out.print("내용 : ");
         String content = sc.nextLine();
 
-        int id = 1;
+        int id = ++lastArticleId;
 
         System.out.printf("%d번 게시물이 등록되었습니다.\n", id);
       }
