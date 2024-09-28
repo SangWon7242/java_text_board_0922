3ba118390c383951ed92ff48e483c120f34d4f95
SangWon7242 <gotkdqja@gmail.com>
2024-09-28 10:44:25 +0900

    Util.getParams 함수 도입

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -2,30 +2,28 @@ import java.util.*;
 
 public class Main {
   public static void main(String[] args) {
-    String queryString = "id=20&subject=제목1&content=내용1&writerName=김철수&hitCount=20";
-    String[] queryStringBits = queryString.split("&");
+    String queryString1 = "id=20&subject=제목1&content=내용1&writerName=김철수&hitCount=20";
+    Map<String, String> params1 = Util.getParams(queryString1);
+    System.out.println(params1);
 
-    Map<String, Object> params = new LinkedHashMap<>();
+    String queryString2 = "id=30&subject=제목2&content=내용2";
+    Map<String, String> params2 = Util.getParams(queryString2);
+    System.out.println(params2);
+  }
+}
 
-    for (String bit : queryStringBits) {
+class Util {
+  static Map<String, String> getParams(String queryStr) {
+    Map<String, String> params = new LinkedHashMap<>();
+
+    String[] queryStrBits = queryStr.split("&");
+
+    for(String bit : queryStrBits) {
       String[] bitBits = bit.split("=");
+
       params.put(bitBits[0], bitBits[1]);
     }
 
-    System.out.println(params);
-
-    System.out.println("== 원하는 것을 하나하나 가져와서 사용 ==");
-    System.out.println("id : " + params.get("id"));
-    System.out.println("subject : " + params.get("subject"));
-    System.out.println("content : " + params.get("content"));
-    System.out.println("writerName : " + params.get("writerName"));
-    System.out.println("hitCount : " + params.get("hitCount"));
-
-    System.out.println("== 반복문을 사용하여 순회 출력 ==");
-    for(String paramName : params.keySet()) {
-      Object paramValue = params.get(paramName);
-
-      System.out.println(paramName + " : " + paramValue);
-    }
+    return params;
   }
 }
