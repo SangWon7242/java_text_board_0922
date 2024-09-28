2030651ba2dbff436929d7966e5ff0562e283993
SangWon7242 <gotkdqja@gmail.com>
2024-09-28 11:46:33 +0900

    url를 으로 기준으로 분리하여 후작업

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -2,26 +2,35 @@ import java.util.*;
 
 public class Main {
   public static void main(String[] args) {
-    String queryString1 = "id=20&subject=제목1&content=내용1&writerName=김철수&hitCount=20";
-    Map<String, String> params1 = Util.getParams(queryString1);
-    System.out.println(params1);
-
-    String queryString2 = "id=30&subject=제목2&content=내용2";
-    Map<String, String> params2 = Util.getParams(queryString2);
-    System.out.println(params2);
+    Map<String, String> params = Util.getParamsFromUrl("/usr/article/write?id=20&subject=제목1&content=내용1&writerName=김철수&hitCount=20");
+    System.out.println(params);
+    System.out.println(params.get("id")); // 20
+    System.out.println(params.get("subject")); // 제목1
+    System.out.println(params.get("content")); // 내용1
+    System.out.println(params.get("writerName")); // 김철수
+    System.out.println(params.get("hitCount")); // 20
   }
 }
 
 class Util {
-  static Map<String, String> getParams(String queryStr) {
-    Map<String, String> params = new LinkedHashMap<>();
+  static Map<String, String> getParamsFromUrl(String url) {
+    Map<String, String> params = new HashMap<>();
+    String[] urlBits = url.split("\\?", 2);
 
-    String[] queryStrBits = queryStr.split("&");
+    if(urlBits.length == 1) {
+      return params;
+    }
 
-    for(String bit : queryStrBits) {
-      String[] bitBits = bit.split("=");
+    String queryString = urlBits[1];
 
-      params.put(bitBits[0], bitBits[1]);
+    for(String bit : queryString.split("&")) {
+      String[] bits = bit.split("=");
+
+      if(bits.length == 1) {
+        continue;
+      }
+
+      params.put(bits[0], bits[1]);
     }
 
     return params;
