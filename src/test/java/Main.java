e44b3fc3081552e791f4c31db0190caa70f7583c
SangWon7242 <gotkdqja@gmail.com>
2024-09-28 09:34:26 +0900

    문자열에서 분리한 데이터를 정수화 시켜서 변수에 저장

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -2,17 +2,31 @@ import java.util.Arrays;
 
 public class Main {
   public static void main(String[] args) {
-    String queryString = "a=1&b=2&c=3";
-    // a=1, b=2, c=3
-    String[] queryStringBits = queryString.split("&");
-    System.out.println(Arrays.toString(queryStringBits));
+    String queryString = "a=100&b=20&c=30";
 
-    for(String bit : queryStringBits) {
+    String[] queryStringBits = queryString.split("&");
+
+    int a = 0;
+    int b = 0;
+    int c = 0;
+
+    for (String bit : queryStringBits) {
       String[] bitBits = bit.split("=");
       String paramName = bitBits[0];
       String paramValue = bitBits[1];
 
-      System.out.printf("%s : %s\n", paramName, paramValue);
+      if (paramName.equals("a")) {
+        a = Integer.parseInt(paramValue);
+      } else if (paramName.equals("b")) {
+        b = Integer.parseInt(paramValue);
+      } else if (paramName.equals("c")) {
+        c = Integer.parseInt(paramValue);
+      }
     }
+
+    System.out.printf("a : %d\n", a);
+    System.out.printf("b : %d\n", b);
+    System.out.printf("c : %d\n", c);
+
   }
 }
