d45b7e9bcf089a8b1356ca2fe06a4ea7e4754614
SangWon7242 <gotkdqja@gmail.com>
2024-09-28 10:18:31 +0900

    쿼리스트링을 분석하여 분리한 데이터를 Map에 저장

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,28 +1,31 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
   public static void main(String[] args) {
-    String queryString = "b=20&c=30&d=70&a=100";
+    String queryString = "id=20&subject=제목1&content=내용1&writerName=김철수&hitCount=20";
     String[] queryStringBits = queryString.split("&");
 
-    List<String> paramNames = new ArrayList<>();
-    List<Integer> paramValues = new ArrayList<>();
+    Map<String, Object> params = new LinkedHashMap<>();
 
     for (String bit : queryStringBits) {
       String[] bitBits = bit.split("=");
-      String paramName = bitBits[0];
-      String paramValue = bitBits[1];
-
-      paramNames.add(paramName);
-      paramValues.add(Integer.parseInt(paramValue));
+      params.put(bitBits[0], bitBits[1]);
     }
 
-    for(int i = 0; i < paramNames.size(); i++) {
-      String paramName = paramNames.get(i);
-      int paramValue = paramValues.get(i);
+    System.out.println(params);
 
-      System.out.printf("%s : %d\n", paramName, paramValue);
+    System.out.println("== 원하는 것을 하나하나 가져와서 사용 ==");
+    System.out.println("id : " + params.get("id"));
+    System.out.println("subject : " + params.get("subject"));
+    System.out.println("content : " + params.get("content"));
+    System.out.println("writerName : " + params.get("writerName"));
+    System.out.println("hitCount : " + params.get("hitCount"));
+
+    System.out.println("== 반복문을 사용하여 순회 출력 ==");
+    for(String paramName : params.keySet()) {
+      Object paramValue = params.get(paramName);
+
+      System.out.println(paramName + " : " + paramValue);
     }
   }
 }
