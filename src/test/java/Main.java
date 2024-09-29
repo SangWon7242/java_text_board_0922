5cf6a21a63f231e287e436740ec2569b09badd18
SangWon7242 <gotkdqja@gmail.com>
2024-09-29 09:14:18 +0900

    Rq 도입

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -2,17 +2,38 @@ import java.util.*;
 
 public class Main {
   public static void main(String[] args) {
-    String url = "/usr/article/write?id=20&subject=제목1&content=내용1&writerName=김철수&hitCount=20";
-    Map<String, String> params = Util.getParamsFromUrl(url);
+    Rq rq = new Rq("/usr/article/write?id=20&subject=제목1&content=내용1&writerName=김철수&hitCount=20");
+    Map<String, String> params = rq.getParams();
     System.out.println(params);
+    System.out.println(rq.getParams());
+    System.out.println(rq.getParams());
 
-    String urlPath = Util.getUrlPathFromUrl(url);
+    String urlPath = rq.getUrlPath();
     System.out.println(urlPath);
+    System.out.println(rq.getUrlPath());
+    System.out.println(rq.getUrlPath());
+  }
+}
+
+class Rq {
+  String url;
+
+  Rq(String url) {
+    this.url = url;
+  }
+
+  public Map<String, String> getParams() {
+    return Util.getParamsFromUrl(url);
+  }
+
+  public String getUrlPath() {
+    return Util.getUrlPathFromUrl(url);
   }
 }
 
 class Util {
   static Map<String, String> getParamsFromUrl(String url) {
+    System.out.println("getParamsFromUrl 실행");
     Map<String, String> params = new LinkedHashMap<>();
     String[] urlBits = url.split("\\?", 2);
 
@@ -36,6 +57,7 @@ class Util {
   }
 
   static String getUrlPathFromUrl(String url) {
+    System.out.println("getUrlPathFromUrl 실행");
     return url.split("\\?", 2)[0];
   }
 }
