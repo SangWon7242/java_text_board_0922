3ae392756e8129d722de043d10482af471ed74fe
SangWon7242 <gotkdqja@gmail.com>
2024-10-06 11:26:30 +0900

    회원 테스트 데이터 구현

--- src/main/java/sbs/com/java/board/member/service/MemberService.java
+++ src/main/java/sbs/com/java/board/member/service/MemberService.java
@@ -3,11 +3,20 @@ package sbs.com.java.board.member.service;
 import sbs.com.java.board.container.Container;
 import sbs.com.java.board.member.repository.MemberRepository;
 
+import java.util.stream.IntStream;
+
 public class MemberService {
     private MemberRepository memberRepository;
 
     public MemberService() {
         memberRepository = Container.memberRepository;
+
+        makeTestData();
+    }
+
+    void makeTestData() {
+        IntStream.rangeClosed(1, 3)
+                .forEach(i -> join("user" + i, "user" + i, "회원" + i));
     }
 
     public void join(String loginId, String loginPw, String name) {
